c51b386a2b7eb7fb5ea6416ee27465d3f2467690
anthimeschrefheere <anthime.schrefheere@gmail.com>
2015-12-03 22:34:14 +0100

    Update urls.py

--- maybee_website/urls.py
+++ maybee_website/urls.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 urlpatterns = patterns('maybee_website.views',
     url(r'^accueil$', 'home'),
 ) 
